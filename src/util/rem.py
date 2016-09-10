cd718ca6d14f0937119b4f7e56115149c8c3c6c5
rdash <rdash@localhost.localdomain>
2016-09-10 17:51:08 -0300

    xml to json done

---
+++ src/util/rem.py
@@ -0,0 +1,115 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import xml.etree.ElementTree as ET
+import json
+
+# main information
+white_list = [u'PRODUCAO']
+
+# specific information
+black1 = u'DADOS-BASICOS'
+black2 = u'AUTORES'
+black_list = [black1,black2]
+
+# wtf
+gray1 = u'TITULO'
+gray2 = u'ANO'
+gray_list = [gray1,gray2]
+
+# convert data to json
+def data2json(name, data):
+	with open(name, 'w') as f:
+		json.dump(data, f, indent=4, sort_keys=True, ensure_ascii=False)
+
+# parse all the shit
+def parser(file):
+	try:
+		tree = ET.parse(file)
+	except Exception as e:
+		print e
+		exit(1)
+	
+	root = tree.getroot()
+	
+	data = {}
+	
+	# STORE THE GENERAL INFORMATIONS
+	# SUCH AS ID, NAME AND CITATION NAMES
+	
+	general_info = {}
+	general_info['ID'] = root.get('NUMERO-IDENTIFICADOR').encode('utf-8')
+	info = root.find('DADOS-GERAIS')
+	general_info['COMPLETE-NAME'] = info.get('NOME-COMPLETO').encode('utf-8')
+	name = info.get('NOME-COMPLETO').replace(" ","") + ".json"
+	general_info['CITATION-NAMES'] = info.get('NOME-EM-CITACOES-BIBLIOGRAFICAS').encode('utf-8')#.split(",")
+	
+	data['GENERAL-INFORMATION'] = general_info
+	articles_data = []
+	# CONTINUE
+	for child in root:
+		
+		ok = next((child.tag for x in white_list if x in child.tag),False)
+		#print child.tag
+		
+		# GO FURTHER
+		if(ok):
+			articles = []
+			rem(child, 0, articles)
+			articles_data = articles_data+articles
+			
+	data['ARTICLES'] = articles_data
+	data2json(name,data)
+
+# FIND ALL THE AUTHORS, BABY
+def rem(node, t, articles):
+	ram = node.findall(u'AUTORES')
+	if(ram):
+		#print node.tag
+		article = {}
+		authors = []
+		for child in node:
+			
+			ok = next((child.tag for x in black_list if x in child.tag),False)
+			
+			if(ok):
+				
+				# IF GENERAL INFORMATION
+				if(black1 in child.tag):
+					# PUBLICATION GENERAL INFORMATION
+					general_info = {}
+					for grand_child in child.keys():
+						if(gray1 in grand_child and u'ING' in grand_child):
+							general_info['ENGLIGH-TITLE'] = child.get(grand_child).encode('utf-8')
+						elif(gray1 in grand_child and u'ING' not in grand_child):
+							general_info['PORTUGUESE-TITLE'] = child.get(grand_child).encode('utf-8')
+						elif(gray2 in grand_child):
+							general_info['DATE'] = child.get(grand_child).encode('utf-8')
+					article['GENERAL-INFORMATION'] = general_info
+				else:
+					author = {}
+					author['COMPLETE-NAME'] = child.get('NOME-COMPLETO-DO-AUTOR').encode('utf-8')
+					author['CITATION-NAMES'] = child.get('NOME-PARA-CITACAO').encode('utf-8')#.split(",")
+					try:
+						author['ID'] = child.get('NRO-ID-CNPQ').encode('utf-8')
+					except:
+						author['ID'] = ""
+						
+					authors.append(author)
+				#print t*'\t' + child.tag, child.attrib
+				#print "\n\n\n"
+				#pass
+		article['AUTHORS'] = authors
+		articles.append(article)
+	else:
+		#print t*'\t' + node.tag
+		
+		for child in node:
+			rem(child, t+1, articles)
+
+
+def main():
+	parser('curriculo22.xml')
+
+if __name__=='__main__':
+	main()
